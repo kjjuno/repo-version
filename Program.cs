2412cc544406eb06d9d72487d0be0c68e7f55951
Kevin Johnson <kljohnson@verisk.com>
2019-07-16 01:48:38 -0600

    Early attempt at an algorithm

--- Program.cs
+++ Program.cs
@@ -1,16 +1,30 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using LibGit2Sharp;
 
 namespace repo_version
 {
-    class Program
-    {
-        static void Main(string[] args)
-        {
-			LibGit2Sharp.Repository r = new LibGit2Sharp.Repository(".");
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			Repository r = new Repository(".");
+
+			Console.WriteLine("Current Branch: {0}", r.Head.FriendlyName);
+
 			foreach (var t in r.Tags)
 			{
-				Console.WriteLine(t.FriendlyName);
+				Console.WriteLine("Last Version: {0}", t.FriendlyName);
+				var filter = new CommitFilter();
+				filter.ExcludeReachableFrom = t.CanonicalName;
+				filter.IncludeReachableFrom = r.Head.Tip.Sha;
+
+				var res = r.Commits.QueryBy(filter).Count();
+				Console.WriteLine("Commits: {0}", res);
 			}
-        }
-    }
+		}
+	}
+
+
 }
