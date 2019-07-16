ba110b1ff333e0f87338fa96d4c92c0ea2248f62
Kevin Johnson <kljohnson@verisk.com>
2019-07-16 01:26:36 -0600

    initial playground

---
+++ Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace repo_version
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+			LibGit2Sharp.Repository r = new LibGit2Sharp.Repository(".");
+			foreach (var t in r.Tags)
+			{
+				Console.WriteLine(t.FriendlyName);
+			}
+        }
+    }
+}
