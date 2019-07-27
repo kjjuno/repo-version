e40da246582fdd2aed3ea060360833bd8869d144
Kevin Johnson <kljohnson@verisk.com>
2019-07-27 04:17:05 +0000

    just about have the basic algorithm

--- Program.cs
+++ Program.cs
@@ -1,30 +1,60 @@
 ﻿using System;
-using System.Collections.Generic;
 using System.Linq;
 using LibGit2Sharp;
+using Version = System.Version;
 
 namespace repo_version
 {
 	class Program
 	{
+		static Version ParseVersion(string input)
+		{
+			var str = input.TrimStart('v', 'V');
+
+			if (!Version.TryParse(str, out var v))
+			{
+				return null;
+			}
+
+			return v;
+		}
+
 		static void Main(string[] args)
 		{
-			Repository r = new Repository(".");
+			Repository r = new Repository("/home/kjjuno/git/claims/services/external-api");
 
 			Console.WriteLine("Current Branch: {0}", r.Head.FriendlyName);
 
-			foreach (var t in r.Tags)
+			var query = from t in r.Tags
+						let v = ParseVersion(t.FriendlyName)
+						where v != null
+						orderby v descending
+						select new
+						{
+							Tag = t,
+							Version = v
+						};
+
+			var latest = query.FirstOrDefault();
+
+			if (latest != null)
 			{
-				Console.WriteLine("Last Version: {0}", t.FriendlyName);
+				Console.WriteLine("Last Version: {0}", latest.Tag.FriendlyName);
 				var filter = new CommitFilter();
-				filter.ExcludeReachableFrom = t.CanonicalName;
+				filter.ExcludeReachableFrom = latest.Tag.CanonicalName;
 				filter.IncludeReachableFrom = r.Head.Tip.Sha;
 
-				var res = r.Commits.QueryBy(filter).Count();
-				Console.WriteLine("Commits: {0}", res);
+				var commits = r.Commits.QueryBy(filter).Count();
+				Console.WriteLine("Commits: {0}", commits);
+				var version = latest.Version;
+
+				if (commits > 0)
+				{
+					version = new Version(version.Major, version.Minor, version.Build + 1);
+				}
+
+				Console.WriteLine("Version: {0}", version);
 			}
 		}
 	}
-
-
 }
