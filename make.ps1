7747b64c5a5ef68bda90e4a29a7fa8bfcf891b5e
Kevin Johnson <kljohnson@verisk.com>
2019-07-29 07:05:36 -0600

    printing the version during make.ps1

--- make.ps1
+++ make.ps1
@@ -2,6 +2,9 @@
 
 $repoVersion = (dotnet run | ConvertFrom-Json)
 
+$fullSemVer = $repoVersion.FullSemVer
+
+Write-Host "FullSemVer: $fullSemVer"
 $env:VERSION = $repoVersion.FullSemVer
 
 dotnet pack -c Release
