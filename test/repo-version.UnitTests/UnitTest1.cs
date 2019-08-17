a6aa57ce5721342f238dc4e7be42ade314753e23
Kevin Johnson <kljohnson@verisk.com>
2019-08-16 22:03:26 -0600

    Adding the beginnings of a unit testing framework

--- test/repo-version.UnitTests/UnitTest1.cs
+++ test/repo-version.UnitTests/UnitTest1.cs
@@ -1,17 +1,26 @@
 using NUnit.Framework;
+using repo_version;
 
 namespace Tests
 {
     public class Tests
     {
+        VersionCalculator versionCalculator;
+
         [SetUp]
         public void Setup()
         {
+            var folderFinder = new GitFolderFinder();
+            var tagFinder = new TaggedVersionFinder();
+            var labelCalculator = new LabelCalculator();
+            versionCalculator = new VersionCalculator(folderFinder, tagFinder, labelCalculator);
         }
 
         [Test]
         public void Test1()
         {
+            var options = Options.Parse(new string[0]);
+            var version = versionCalculator.CalculateVersion(options);
             Assert.Pass();
         }
     }
