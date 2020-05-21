eb601de4c55da623865e7a32b0bd196a6e6c70c8
Preston Williams <prwilliams@xactware.com>
2020-05-21 13:32:02 -0600

    Removing things that didn't need to be in this PR

--- src/publish.ps1
+++
@@ -1,3 +0,0 @@
-dotnet publish ./repo-version/repo-version.csproj -c Release -f netcoreapp2.1 -o ../artifacts/linux -r linux-x64
-dotnet publish ./repo-version/repo-version.csproj -c Release -f netcoreapp2.1 -o ../artifacts/windows -r win10-x64
-dotnet publish ./repo-version/repo-version.csproj -c Release -f netcoreapp2.1 -o ../artifacts/macOS -r osx.10.10-x64
